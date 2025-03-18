fe3ccdf7c6349d515ce4568de2e5a0297fa63f6e
shivam chikara <s.chikara6885@gmail.com>
2025-03-19 01:21:46 +0530

    add the new chat button and its functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,12 +14,25 @@ const Sidebar = ({ expand, setExpand }) => {
                         <Image className='md:hidden' src={assets.menu_icon} alt="" />
                         <Image className='hidden md:block w-7' src={expand ? assets.sidebar_close_icon : assets.sidebar_icon} alt="" />
 
-                        <div>
+                        <div className={`absolute w-max ${expand ? 'left-1/2 -translate-x-1/2 top-12' : '-top-12 left-0'} opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}>
                             {expand ? 'Close sidebar' : 'Open sidebar'}
                             <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? 'left-1/2 -top-1.5 -translate-x-1/2' : 'left-4 -bottom-1.5'}`}></div>
                         </div>
                     </div>
                 </div>
+
+
+                <button className={`mt-8 flex items-center justify-center cursor-pointer ${expand ? 'bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max' : 'group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg'}`}>
+                    <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt=''/>
+                    <div className='absolute w-max -top-12 -right-12 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none'>
+                        New chat
+                        <div className='w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5'>
+
+                        </div>
+                    </div>
+
+                    {expand && <p className='text-white text font-medium'>New chat</p>}
+                </button>
             </div>
         </div>
     )
